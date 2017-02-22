89d5b0c286dd849ccd61b63219299bc0a0a4e1fc
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-02-21 23:05:39 -0800

    Add logging. Only info and above will be logged.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-agent", "Elixir test@email.com"}]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -13,10 +17,15 @@ defmodule Issues.GithubIssues do
   end
 
   defp handle_response({:ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
+
     {:ok, parse_response(body)}
   end
 
-  defp handle_response({_, %{status_code: _, body: body}}) do
+  defp handle_response({_, %{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
+
     {:error, parse_response(body)}
   end
 
